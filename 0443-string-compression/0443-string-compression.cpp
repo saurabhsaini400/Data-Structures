07af7939e17e7dff7ed8a6735e8138837ed102b8
SAURABH SAINI <55580746+saurabhsaini400@users.noreply.github.com>
2023-03-02 15:50:16 +0530

    Time: 6 ms (53.56%), Space: 8.8 MB (98.13%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int n = chars.size();
-        int i, j, decrement = 0;
-        i = j = n-1;
-        while(i >= 0) {
-            while(j >= 0 and chars[j] == chars[i]) j--;
-            int freq = i-j;
-            if(freq == 1) {
-                i = j;
-                continue;
-            }
-            int dec = 0;
-            dec += freq-1;
-            dec -= (int)(log10(freq)+1);
-            decrement += dec;
-            string strfreq = to_string(freq);
-            int k = 0;
-            for(int l = j+2; l < n-decrement; l++) {
-                if(k < strfreq.length()) chars[l] = strfreq[k++];
-                else chars[l] = chars[l+dec];
-            }
-            
-            i = j;
+    int n = chars.size();
+    int i = 0, j = 0;
+    while (i < n) {
+        int count = 1;
+        while (i + 1 < n && chars[i] == chars[i+1]) {
+            count++;
+            i++;
         }
-        
-        return n-decrement;
+        chars[j] = chars[i];
+        j++;
+        if (count > 1) {
+            string s = to_string(count);
+            for (int k = 0; k < s.length(); k++) {
+                chars[j] = s[k];
+                j++;
+            }
+        }
+        i++;
     }
+    return j;
+}
 };
